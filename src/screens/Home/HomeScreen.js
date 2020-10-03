90ddb3e845f5ed515b840a15102d404b0ff0eccd
gonzalo.rivero <gonzalo.rivero@naranjax.com>
2020-10-03 20:26:57 -0300

    agrego nueva home

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -3,56 +3,8 @@ import {  Text, View, StyleSheet, Dimensions, ScrollView, TouchableHighlight,Tou
 import styles from './styles';
 import MenuImage from '../../components/MenuImage/MenuImage';
 
-import DashboardService from '../../service/DashboardService';
-
-import ButtonHome from './Buttons'
-
-import {
-  BarChart,
-  LineChart,
-  PieChart, ProgressChart,
-  ContributionGraph,
-  StackedBarChart,
-  } from 'react-native-chart-kit';
-
-const screenWidth = Dimensions.get("window").width;
-const chartConfig = {
-  backgroundColor: '#136616',
-  backgroundGradientFromOpacity: 0,
-  backgroundGradientFrom: '#9edba0',
-  backgroundGradientToOpacity: 0.5,
-  backgroundGradientTo: '#4ce651',
-  color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`
-};
-const graphStyle = {
-  marginVertical: 8,
-  borderRadius: 16
-}
-const chartConfig1 = {
-  backgroundGradientFrom: "#1E2923",
-  backgroundGradientFromOpacity: 0,
-  backgroundGradientTo: "#08130D",
-  backgroundGradientToOpacity: 0.5,
-  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-  strokeWidth: 2, // optional, default 3
-  barPercentage: 0.5,
-  showBarTops: true,
-  useShadowColorFromDataset: false // optional
-};
-const data = {
-  labels: ["January", "February", "March", "April", "May", "June"],
-  datasets: [
-    {
-      data: [20, 45, 28, 80, 99, 43],
-      color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
-      strokeWidth: 2 // optional
-    }
-  ],
-  legend: ["Rainy Days"] // optional
-};
-
-
 export default class HomeScreen extends React.Component {
+
   static navigationOptions = ({ navigation }) => ({
     title: 'Home',
     headerLeft: () => <MenuImage
@@ -64,31 +16,74 @@ export default class HomeScreen extends React.Component {
 
   constructor(props) {
     super(props);
-    this.dashboardService = new DashboardService();
+    console.log(props);
     this.state =  {
-                  accounts:[],
-                  balances:[]
+        accounts:[],
+        balances:[]
     };
   }
-
 
   render() {
     const { navigation } = this.props;
     return (
-      <View style={{backgroundColor:"white"}} >
+      <View style={{backgroundColor:"#EBEDEB"}} >
         <ScrollView style={styles.mainContainer}>
-        <TouchableHighlight             onPress={() => {
-              navigation.navigate('Cuentas',{name: 'Cuentas'});
-              }}>
-        <Image style={{width: 350, height:250 }}  source={require('../../data/bpayapp.jpeg')} />
-        </TouchableHighlight>
-        <View>
-        <Image style={{width: 400, height:100 }}  source={require('../../data/budgetbanner.jpeg')} />
-        <Text style={{fontWeight:"bold", fontSize: 15 }}>Gestiona tus productos </Text>
+        <View style={{alignItems: 'center', justifyContent: 'center', height: 150, backgroundColor: 'white'}}>
+            <Text style={{fontWeight: 'bold', fontSize: 20}}>¡Bienvenido a My Budget!</Text>
+            <Text style={{fontSize: 20, margin: 10 }}>Comenzá a administrar tu economía personal de la forma más fácil</Text>
         </View>
-        <View style={styles.buttonStyle}>
-      </View>
-      <ButtonHome></ButtonHome>
+        <View>
+              <View style={styles.buttonContainer}>
+                  <View style={styles.largeButton}>
+                    <TouchableHighlight
+                        onPress={ () => {
+                            navigation.navigate('Dashboard');
+                        } }>
+                        <View>
+                            <Image source={require('../../../assets/icons/home-button-dashboard.jpg')} style={styles.homeButtonLargeImage}/>
+                            <Text style={styles.homeButtonText}>RESUMEN GENERAL</Text>
+                        </View>
+                    </TouchableHighlight>
+                  </View>
+              </View>
+              <View style={styles.buttonContainer}>
+                <View style={styles.squareButton}>
+                    <TouchableHighlight
+                        onPress={ () => {
+                            navigation.navigate('Income');
+                        } }>
+                        <View>
+                            <Image source={require('../../../assets/icons/home-button-income.png')} style={{height:64, width: 64}} />
+                            <Text style={styles.homeButtonText}>INGRESOS</Text>
+                        </View>
+                    </TouchableHighlight>
+                </View>
+                <View style={styles.squareButton}>
+                    <TouchableHighlight
+                        onPress={ () => {
+                            navigation.navigate('Expenses');
+                        } }>
+                        <View>
+                            <Image source={require('../../../assets/icons/home-button-expense.png')} style={{height:64, width: 64}} />
+                            <Text style={styles.homeButtonText}>EGRESOS</Text>
+                        </View>
+                    </TouchableHighlight>
+                </View>
+              </View>
+              <View style={styles.buttonContainer}>
+                  <View style={styles.largeButton}>
+                    <TouchableHighlight
+                    onPress={ () => {
+                        navigation.navigate('Presupuesto');
+                    } }>
+                        <View>
+                            <Image source={require('../../../assets/icons/home-button-budget.jpg')} style={styles.homeButtonLargeImage}/>
+                            <Text style={styles.homeButtonText}>PRESUPUESTO</Text>
+                        </View>
+                    </TouchableHighlight>
+                  </View>
+              </View>
+            </View>
       </ScrollView>
       </View>
     )
